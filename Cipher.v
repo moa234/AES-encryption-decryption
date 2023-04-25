e0e2953c56b4de8afcbe841d222931feca29daf7
Moaaz Tarek <moaaztarek@hotmail.com>
2023-04-25 06:28:55 +0200

    cipher full

--- Cipher.v
+++ Cipher.v
@@ -1,14 +1,21 @@
-module Cipher (
+module Cipher #(parameter Nk = 4, Nr = 10)(
     input [127:0] data_in,
-    input [127:0] key,
+    input [Nk * 32 - 1:0] key,
     output reg[127:0] data_out
 );
-reg [127:0] state[39:0];
-// AddRoundKey addr (
-//     .data_in(data_in),
-//     //.key(key),
-//     .data_out(state[0])
-// );
+wire [127:0] state[39:0];
+wire [(Nr + 1) * 128 - 1:0] w;
+
+KeyExpansion keyexp (
+    .key_in(key),
+    .key_out(w)
+);
+
+AddRoundKey addr (
+    .data_in(data_in),
+    .key(w[(Nr + 1) * 128 - 1 -: 128]),
+    .data_out(state[0])
+);
 
 genvar i;
 for (i = 0;i < 9; i = i + 1) begin
@@ -26,7 +33,7 @@ for (i = 0;i < 9; i = i + 1) begin
     );
     AddRoundKey addr (
         .data_in(state[i * 4 + 3]),
-        .key(key),
+        .key(w[(Nr + 1) * 128 - 1 - (i + 1) * 128 -: 128]),
         .data_out(state[i * 4 + 4])
     );
 end
@@ -38,11 +45,12 @@ ShiftRows shiftr2 (
     .data_in(state[37]),
     .data_out(state[38])
 );
-// AddRoundKey addr2 (
-//     .data_in(state[38]),
-//     .key(key),
-//     .data_out(data_out)
-// );
+AddRoundKey addr2 (
+    .data_in(state[38]),
+    .key(w[127 : 0]),
+    .data_out(state[39])
+);
 
-    
+assign data_out = state[39];
+
 endmodule
