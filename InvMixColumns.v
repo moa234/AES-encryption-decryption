0da67501ef9c8851d8d67c9c7b3b492856478e05
Moaaz Tarek <moaaztarek@hotmail.com>
2023-04-25 03:30:07 +0200

    fix invmixcolumns

--- InvMixColumns.v
+++ InvMixColumns.v
@@ -1,130 +1,46 @@
-module MixColumns (
+module InvMixColumns (
     input [127:0] data_in,
     output reg[127:0] data_out
 );
-
-reg [7:0] mule;
-reg [7:0] muleout;
-reg [7:0] mulb;
-reg [7:0] mulbout;
-reg [7:0] muld;
-reg [7:0] muldout;
-reg [7:0] mul9;
-reg [7:0] mul9out;
-
-GF28mult #(.N(14)) multe (
-    .data_in(mule),
-    .data_out(muleout)
-);
-GF28mult #(.N(11)) multb (
-    .data_in(mulb),
-    .data_out(mulbout)
-);
-GF28mult #(.N(13)) multd (
-    .data_in(muld),
-    .data_out(muldout)
-);
-GF28mult #(.N(9)) mult9 (
-    .data_in(mul9),
-    .data_out(mul9out)
-);
-
+integer i;
 always @(data_in) begin
-    mule = data_in[127 -: 8];
-    mulb = data_in[119 -: 8];
-    muld = data_in[111 -: 8];
-    mul9 = data_in[103 -: 8];
-    data_out[127 -: 8] = muleout ^ mulbout ^ muldout ^ mul9out;
+    for (i = 0; i < 4; i = i + 1) begin
+        data_out[127 - 32*i -: 8] = GF28mul('he,data_in[127 - 32*i -: 8]) ^ GF28mul('hb,data_in[119 - 32*i-: 8]) 
+         ^ GF28mul('hd,data_in[111 - 32*i -: 8]) ^ GF28mul('h9,data_in[103 - 32*i -: 8]);
 
-    mul9 = data_in[127 -: 8];
-    mule = data_in[119 -: 8];
-    mulb = data_in[111 -: 8];
-    muld = data_in[103 -: 8];
-    data_out[119 -: 8] = muleout ^ mulbout ^ muldout ^ mul9out;
+        data_out[119 - 32*i -: 8] = GF28mul('h9,data_in[127 - 32*i -: 8]) ^ GF28mul('he,data_in[119 - 32*i-: 8]) 
+         ^ GF28mul('hb,data_in[111 - 32*i -: 8]) ^ GF28mul('hd,data_in[103 - 32*i -: 8]);
 
-    muld = data_in[127 -: 8];
-    mul9 = data_in[119 -: 8];
-    mule = data_in[111 -: 8];
-    mulb = data_in[103 -: 8];
-    data_out[111 -: 8] = muleout ^ mulbout ^ muldout ^ mul9out;
+        data_out[111 - 32*i -: 8] = GF28mul('hd,data_in[127 - 32*i -: 8]) ^ GF28mul('h9,data_in[119 - 32*i-: 8]) 
+         ^ GF28mul('he,data_in[111 - 32*i -: 8]) ^ GF28mul('hb,data_in[103 - 32*i -: 8]);
 
-    mulb = data_in[127 -: 8];
-    muld = data_in[119 -: 8];
-    mul9 = data_in[111 -: 8];
-    mule = data_in[103 -: 8];
-    data_out[103 -: 8] = muleout ^ mulbout ^ muldout ^ mul9out;
-
-    mule = data_in[95 -: 8];
-    mulb = data_in[87 -: 8];
-    muld = data_in[79 -: 8];
-    mul9 = data_in[71 -: 8];
-    data_out[95 -: 8] = muleout ^ mulbout ^ muldout ^ mul9out;
-
-    mul9 = data_in[95 -: 8];
-    mule = data_in[87 -: 8];
-    mulb = data_in[79 -: 8];
-    muld = data_in[71 -: 8];
-    data_out[87 -: 8] = muleout ^ mulbout ^ muldout ^ mul9out;
-
-    muld = data_in[95 -: 8];
-    mul9 = data_in[87 -: 8];
-    mule = data_in[79 -: 8];
-    mulb = data_in[71 -: 8];
-    data_out[79 -: 8] = muleout ^ mulbout ^ muldout ^ mul9out;
-
-    mulb = data_in[95 -: 8];
-    muld = data_in[87 -: 8];
-    mul9 = data_in[79 -: 8];
-    mule = data_in[71 -: 8];
-    data_out[71 -: 8] = muleout ^ mulbout ^ muldout ^ mul9out;
-
-    mule = data_in[63 -: 8];
-    mulb = data_in[55 -: 8];
-    muld = data_in[47 -: 8];
-    mul9 = data_in[39 -: 8];
-    data_out[63 -: 8] = muleout ^ mulbout ^ muldout ^ mul9out;
-
-    mul9 = data_in[63 -: 8];
-    mule = data_in[55 -: 8];
-    mulb = data_in[47 -: 8];
-    muld = data_in[39 -: 8];
-    data_out[55 -: 8] = muleout ^ mulbout ^ muldout ^ mul9out;
-
-    muld = data_in[63 -: 8];
-    mul9 = data_in[55 -: 8];
-    mule = data_in[47 -: 8];
-    mulb = data_in[39 -: 8];
-    data_out[47 -: 8] = muleout ^ mulbout ^ muldout ^ mul9out;
-
-    mulb = data_in[63 -: 8];
-    muld = data_in[55 -: 8];
-    mul9 = data_in[47 -: 8];
-    mule = data_in[39 -: 8];
-    data_out[39 -: 8] = muleout ^ mulbout ^ muldout ^ mul9out;
-
-    mule = data_in[31 -: 8];
-    mulb = data_in[23 -: 8];
-    muld = data_in[15 -: 8];
-    mul9 = data_in[7 -: 8];
-    data_out[31 -: 8] = muleout ^ mulbout ^ muldout ^ mul9out;
-
-    mul9 = data_in[31 -: 8];
-    mule = data_in[23 -: 8];
-    mulb = data_in[15 -: 8];
-    muld = data_in[7 -: 8];
-    data_out[23 -: 8] = muleout ^ mulbout ^ muldout ^ mul9out;
-
-    muld = data_in[31 -: 8];
-    mul9 = data_in[23 -: 8];
-    mule = data_in[15 -: 8];
-    mulb = data_in[7 -: 8];
-    data_out[15 -: 8] = muleout ^ mulbout ^ muldout ^ mul9out;
-
-    mulb = data_in[31 -: 8];
-    muld = data_in[23 -: 8];
-    mul9 = data_in[15 -: 8];
-    mule = data_in[7 -: 8];
-    data_out[7 -: 8] = muleout ^ mulbout ^ muldout ^ mul9out;
+        data_out[103 - 32*i -: 8] = GF28mul('hb,data_in[127 - 32*i -: 8]) ^ GF28mul('hd,data_in[119 - 32*i-: 8]) 
+         ^ GF28mul('h9,data_in[111 - 32*i -: 8]) ^ GF28mul('he,data_in[103 - 32*i -: 8]);
+    end
 end
+
+function [7:0] GF28mul;
+    input [7:0] a, b;
+    reg [7:0] p;
+    reg ahi;
+    integer i;
+    begin
+        p = 8'b0;
+        for (i = 0; i < 8; i = i + 1)
+        begin
+            if (b[0] == 1'b1)
+            begin
+                p = a ^ p;
+            end
+            ahi = a[7];
+            a = a << 1;
+            if (ahi == 1'b1) begin
+                a = a ^ 8'h1b;
+            end
+            b = b >> 1;
+        end
+        GF28mul = p;
+    end
+endfunction 
 
 endmodule
